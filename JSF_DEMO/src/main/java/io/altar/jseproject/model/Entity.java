f8875c807b9c51fc17333a814dde38919e39a948
Tiago-Cruz <tiago_c5@hotmail.com>
2017-06-19 13:22:12 +0100

    Commit 1

---
+++ JSF_DEMO/src/main/java/io/altar/jseproject/model/Entity.java
@@ -0,0 +1,14 @@
+package io.altar.jseproject.model;
+
+public class Entity {
+	
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
