307a7fe85b2e3251c6144759f9fb0d565d6f1b1c
Tiago-Cruz <tiago_c5@hotmail.com>
2017-06-26 12:53:30 +0100

    Commit CDI

--- JSF_DEMO/src/main/java/io/altar/jseproject/model/Product.java
+++ JSF_DEMO/src/main/java/io/altar/jseproject/model/Product.java
@@ -1,5 +1,44 @@
 package io.altar.jseproject.model;
 
-public class Product {
+import java.util.ArrayList;
+import io.altar.jseproject.repository.ProductRepository;
 
+public class Product extends Entity {
+	
+	private ArrayList<Integer> shelfIdLocation;
+	private String name;
+	private Integer discount;
+	private Integer iva;
+	private Double pvp;
+	
+	public ArrayList<Integer> getShelfIdLocation() {
+		return shelfIdLocation;
+	}
+	public void setShelfIdLocation(ArrayList<Integer> shelfIdLocation) {
+		this.shelfIdLocation = shelfIdLocation;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getDiscount() {
+		return discount;
+	}
+	public void setDiscount(Integer discount) {
+		this.discount = discount;
+	}
+	public Integer getIva() {
+		return iva;
+	}
+	public void setIva(Integer iva) {
+		this.iva = iva;
+	}
+	public Double getPvp() {
+		return pvp;
+	}
+	public void setPvp(Double pvp) {
+		this.pvp = pvp;
+	}
 }
---
+++ JSF_DEMO/src/main/java/io/altar/jseproject/service/ShelfService.java
@@ -0,0 +1,5 @@
+package io.altar.jseproject.service;
+
+public class ShelfService {
+
+}
